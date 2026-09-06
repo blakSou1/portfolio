4d831e7bd807d29f3d6254e471144e510d86e60a
blakSou1 <portfolio@local>
2026-09-07 02:30:52 +0500

    fix: import viewer.js directly to avoid version mismatch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-﻿import { openModelViewer, openShaderViewer, mountBackground, renderModelThumbnail } from "./viewer.min.js?v=20260906i";
+﻿import { openModelViewer, openShaderViewer, mountBackground, renderModelThumbnail } from "./viewer.js?v=20260906n";
 
 // GitHub Pages ставит долгий cache-control на статику. Чтобы браузер НЕ хранил
 // старые файлы, к URL подставляем "v". Для ассетов (модели, рендеры) берём blob-SHA
